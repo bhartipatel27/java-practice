5f644815f0f8b419aeea0ee873eaba595bc14e52
bhartipatel27 <patelbharu27@gmail.com>
2022-10-01 17:36:44 +0100

    My first homework

---
+++ src/helloworld/PrintNumber.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class PrintNumber {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        String name = "Print";
+
+        System.out.println(a); //10
+        System.out.println(name); //Print
+
+    }
+}
